33a4f5bb8458c49774ad7f22491ddeb31e012a58
Ashwinth Anbu <ashwinth04@gmail.com>
2024-04-21 15:32:17 +0530

    Added Simulated Annealing and modularized the folder structure

--- main.py
+++ main.py
@@ -1,189 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import copy
-
-nodes = {
-    1:{
-        "Node ID": 1,
-        "Position": (100, 200),
-        "Neighbors": [2,3,4,5],
-        "Initial Energy": 100,
-        "Residual Energy": 95,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    2:{
-        "Node ID": 2,
-        "Position": (50,150),
-        "Neighbors": [6,7],
-        "Initial Energy": 98,
-        "Residual Energy": 90,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    3:{
-        "Node ID": 3,
-        "Position": (100,150),
-        "Neighbors": [8,9],
-        "Initial Energy": 95,
-        "Residual Energy": 95,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    4:{
-        "Node ID": 4,
-        "Position": (150,150),
-        "Neighbors": [9,10],
-        "Initial Energy": 90,
-        "Residual Energy": 75,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    5:{
-        "Node ID": 5,
-        "Position": (200,150),
-        "Neighbors": [10,11],
-        "Initial Energy": 85,
-        "Residual Energy": 25,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    6:{
-        "Node ID": 6,
-        "Position": (33,100),
-        "Neighbors": [12],
-        "Initial Energy": 83,
-        "Residual Energy": 50,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    7:{
-        "Node ID": 7,
-        "Position": (66,100),
-        "Neighbors": [12],
-        "Initial Energy": 87,
-        "Residual Energy": 77,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    8:{
-        "Node ID": 8,
-        "Position": (99,100),
-        "Neighbors": [13],
-        "Initial Energy": 73,
-        "Residual Energy": 70,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    9:{
-        "Node ID": 9,
-        "Position": (133,100),
-        "Neighbors": [13],
-        "Initial Energy": 65,
-        "Residual Energy": 62,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    10:{
-        "Node ID": 10,
-        "Position": (166,100),
-        "Neighbors": [14],
-        "Initial Energy": 60,
-        "Residual Energy": 30,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    11:{
-        "Node ID": 11,
-        "Position": (199,100),
-        "Neighbors": [14],
-        "Initial Energy": 55,
-        "Residual Energy": 55,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    12:{
-        "Node ID": 12,
-        "Position": (75,50),
-        "Neighbors": [15],
-        "Initial Energy": 30,
-        "Residual Energy": 25,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    13:{
-        "Node ID": 13,
-        "Position": (140,50),
-        "Neighbors": [15],
-        "Initial Energy": 45,
-        "Residual Energy": 45,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    14:{
-        "Node ID": 14,
-        "Position": (185,50),
-        "Neighbors": [15],
-        "Initial Energy": 38,
-        "Residual Energy": 38,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    },
-    15:{
-        "Node ID": 15,
-        "Position": (100,10),
-        "Neighbors": [],
-        "Initial Energy": 35,
-        "Residual Energy": 22,
-        "PRR": np.random.uniform(0.8, 0.91),
-        "Buffer Capacity": np.random.randint(8, 11),
-        "Free Buffer": np.random.randint(8, 11),
-        "Data Packet": {},
-        "PTR": 0.88,
-    }
-}
+from numpy import random
+from nodes import nodes
 
 def calculate_heuristic(node,sink):
     node_pos = nodes[node]["Position"]
@@ -272,26 +91,78 @@ y_coords = []
 
 
 
-for node,node_dict in nodes.items():
-    x_coords.append(node_dict["Position"][0])
-    y_coords.append(node_dict["Position"][1])
+# for node,node_dict in nodes.items():
+#     x_coords.append(node_dict["Position"][0])
+#     y_coords.append(node_dict["Position"][1])
 
-plt.scatter(x_coords,y_coords)
+# plt.scatter(x_coords,y_coords)
 
-for node,node_dict in nodes.items():
-    first = node_dict["Position"]
-    for n in node_dict["Neighbors"]:
-        second = nodes[n]["Position"]
-        plt.plot([first[0],second[0]],[first[1],second[1]])
+# for node,node_dict in nodes.items():
+#     first = node_dict["Position"]
+#     for n in node_dict["Neighbors"]:
+#         second = nodes[n]["Position"]
+#         plt.plot([first[0],second[0]],[first[1],second[1]])
 
-plt.show()
+# plt.show()
 
-path = [1,3,8,13,15]
-print(f"Path = {path}, Residual Energy of network if this path is taken: {calculate_energy_for_path(path)}")
+# path = [1,3,8,13,15]
+# print(f"Path = {path}, Residual Energy of network if this path is taken: {calculate_energy_for_path(path)}")
 
-path = [1,2,7,12,15]
-print(f"Path = {path}, Residual Energy of network if this path is taken: {calculate_energy_for_path(path)}")
+# path = [1,2,7,12,15]
+# print(f"Path = {path}, Residual Energy of network if this path is taken: {calculate_energy_for_path(path)}")
 
-print("Initial energy: ",calculate_total_energy(nodes))
-print(astar(1,15))
-print("Final energy: ",calculate_total_energy(nodes))
+# print("Initial energy: ",calculate_total_energy(nodes))
+# print(astar(1,15))
+# print("Final energy: ",calculate_total_energy(nodes))
+
+
+def generate_random_path(source,sink):
+    path = []
+    path.append(source)
+    node = path[-1]
+    while(node != sink):
+        neighbours = nodes[node]["Neighbors"]
+        successor = random.choice(neighbours)
+        path.append(successor)
+        node = path[-1]
+
+    return path
+
+def find_different_path(path):
+    index = random.randint(1,len(path)-1)
+    prev_index = index - 1
+    neighbors = nodes[path[prev_index]]["Neighbors"]
+    new_bit = random.choice(neighbors)
+    path[index] = new_bit
+    for i in range(index+1,len(path)):
+        if(path[i] not in nodes[path[i-1]]["Neighbors"]):
+            path[i] = random.choice(nodes[path[i-1]]["Neighbors"])
+
+    return path
+
+# print(find_different_path([1,3,8,13,15]))
+
+
+def simulated_annealing(initial_path,temperature, cooling_rate):
+    path = initial_path
+    while(temperature > 0.01):
+        new_path = find_different_path(path)
+        new_cost = calculate_energy_for_path(new_path)
+        old_cost = calculate_energy_for_path(path)
+        if(new_cost > old_cost):
+            path = new_path
+            continue
+        else:
+            dele = old_cost - new_cost
+            p = np.exp(-1*dele/temperature)
+            r = random.randint(0,1)
+            if(p > r):
+                path = new_path
+            temperature= cooling_rate*temperature
+
+    return path
+
+initial_path = [1, 5, 11, 14, 15]
+print(f"Initial path: {initial_path}, Energy if this path was taken = {calculate_energy_for_path(initial_path)}")
+path = simulated_annealing(initial_path,1000,0.1)
+print(f"Path: {path}, Residual Energy = {calculate_energy_for_path(path)}")
